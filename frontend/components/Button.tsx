ad44d6c3a3db8e503583cb4d22f35c8d509ce9d1
Seikirin <seikirin.tempest@gmail.com>
2023-12-04 13:15:22 +0100

    frontend initialized 👍

---
+++ frontend/components/Button.tsx
@@ -0,0 +1,43 @@
+export function Button({
+	children,
+	iconOnly = false,
+	startContent,
+	variant = "default",
+	...props
+}: {
+	children?: any;
+	iconOnly?: boolean;
+	startContent?: React.ReactNode;
+	variant?: "default" | "secondary" | "danger" | "transparent" | "ghost";
+	[key: string]: any;
+}) {
+	return (
+		<button
+			data-variant={variant}
+			data-start-content={!!startContent}
+			data-icon-only={iconOnly}
+			className="flex items-center justify-center gap-2 
+                rounded-3xl bg-gradient-to-t from-primary-400 to-primary-600 p-2
+                px-4 text-sm shadow
+                transition-all
+                duration-300
+                focus-within:ring-2 focus-within:ring-primary-400 hover:brightness-110
+                focus:outline-none active:scale-95 data-[variant=danger]:from-red-700
+                data-[variant=ghost]:from-white/10 data-[variant=secondary]:from-secondary-400 data-[variant=transparent]:from-white/0 
+                data-[variant=danger]:to-red-500 data-[variant=ghost]:to-white/10
+                data-[variant=secondary]:to-secondary-600
+                data-[variant=transparent]:to-white/0
+                data-[icon-only=true]:p-2
+                data-[variant=danger]:text-white data-[variant=ghost]:text-white data-[variant=secondary]:text-white data-[variant=transparent]:text-white
+                data-[variant=transparent]:shadow-none
+                data-[variant=transparent]:hover:from-white/10
+                data-[variant=transparent]:hover:to-white/10
+                truncate
+                "
+			{...props}
+		>
+			{startContent}
+			{children}
+		</button>
+	);
+}
---
+++ frontend/components/Navbar.tsx
@@ -0,0 +1,96 @@
+"use client";
+
+import { LogIn, Search, UserCircle2 } from "lucide-react";
+import { Button } from "@/components/Button";
+import { useEffect, useState } from "react";
+
+const buttons = ["Home", "Leaderboard", "Play"] as const;
+
+function Navigation() {
+	const [active, setActive] = useState(buttons[0]) as [
+		(typeof buttons)[number],
+		(button: (typeof buttons)[number]) => void,
+	];
+
+	return (
+		<>
+			{buttons.map((button, i) => (
+				<Button
+					variant={button === active ? "default" : "transparent"}
+					key={button}
+					onClick={() => setActive(button)}
+				>
+					{button}
+				</Button>
+			))}
+		</>
+	);
+}
+
+function LoginButton() {
+	return (
+		<Button
+			startContent={<img className="h-5 w-5 invert" src="42_Logo.svg" />}
+			variant="ghost"
+		>
+			Sign in
+		</Button>
+	);
+}
+
+function SearchBar() {
+	return (
+		<div className="flex h-full w-full items-center justify-center gap-2 rounded-3xl bg-white/10 px-4 pl-2 text-sm text-white shadow transition-all duration-300 file:h-full focus-within:ring-2 focus-within:ring-primary-400 hover:brightness-110 active:scale-95">
+			<Search className="h-2/4 text-white/50" />
+			<input
+				className="flex-1 bg-transparent text-white placeholder-white/50 outline-none"
+				placeholder="Search"
+			/>
+		</div>
+	);
+}
+
+export function Navbar() {
+	const [solid, setSolid] = useState(false);
+
+	useEffect(() => {
+		const listener = () => {
+			setSolid(window.scrollY > 25);
+		};
+		window.addEventListener("scroll", listener);
+		return () => window.removeEventListener("scroll", listener);
+	}, []);
+
+	return (
+		<nav
+			data-solid={solid}
+			className="fixed top-0 z-50 flex h-28 w-full items-center justify-center px-8
+				data-[solid=true]:px-0
+				data-[solid=true]:h-16 transition-all
+				ease-in-out
+				duration-300
+				">
+			<div
+				data-solid={solid}
+				className="flex w-3/4 rounded-full bg-black/50 shadow-lg shadow-card/25 transition-all items-center justify-center
+				data-[solid=true]:bg-black data-[solid=true]:bg-opacity-100 data-[solid=true]:backdrop-blur-sm data-[solid=true]:w-full data-[solid=true]:h-full data-[solid=true]:rounded-none
+				data-[solid=true]:px-8
+				ease-in-out
+				duration-300
+				"
+			>
+				<div className="m-2 flex h-9 w-full items-center justify-between gap-2">
+					<div className="flex h-full flex-1 items-center justify-start gap-2">
+						<SearchBar />
+					</div>
+					<div className="flex h-full flex-1 items-center justify-center gap-2">
+						<Navigation />
+					</div>
+					<div className="flex h-full flex-1 items-center justify-end gap-2">
+						<LoginButton />
+					</div>
+				</div>
+			</div>
+		</nav>
+	);
+}
