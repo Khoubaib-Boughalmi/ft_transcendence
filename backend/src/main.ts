8dea51c41aea82cbc67edeb5437ee91c690158fa
NotAsheraf <ash@rian.ma>
2023-12-17 10:39:06 +0100

    Update main.ts with global pipes and cookie parser

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('/api');
-  app.enableCors({ origin: '*' });
-  await app.listen(3000);
+	const app = await NestFactory.create(AppModule);
+	app.setGlobalPrefix('/api');
+	app.enableCors({ origin: process.env.FRONTEND_URL, credentials: true });
+	app.useGlobalPipes(new ValidationPipe({
+		forbidNonWhitelisted: true,
+		transform: true,
+		whitelist: true,
+	}));
+	app.use(cookieParser());
+	await app.listen(3000);
 }
 bootstrap();
