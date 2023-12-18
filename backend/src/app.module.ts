03ac0dfb24187daaf6d38a5e137e60bc5c3fcfe0
NotAsheraf <ash@rian.ma>
2023-12-18 16:10:05 +0100

    Add joi package and update validation schema for env

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,10 +4,24 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import * as Joi from 'joi';
 
 @Module({
 	imports: [
-		ConfigModule.forRoot({ isGlobal: true }),
+		ConfigModule.forRoot({
+			isGlobal: true,
+			validationSchema: Joi.object({
+				FRONTEND_URL: Joi.string().required(),
+				INTRA_CLIENT_ID: Joi.string().required(),
+				INTRA_CLIENT_SECRET: Joi.string().required(),
+				INTRA_CALLBACK_URL: Joi.string().required(),
+				JWT_SECRET: Joi.string().required(),
+				DATABASE_URL: Joi.string().required(),
+			}),
+			validationOptions: {
+				abortEarly: true,
+			},
+		}),
 		AuthModule,
 		UserModule,
 	],
