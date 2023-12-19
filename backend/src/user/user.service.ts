eb6efb7b998d7de6fd722c1e1123364446b80eaa
NotAsheraf <ash@rian.ma>
2023-12-19 11:07:58 +0100

    Add GameMatch model and update User model and UserService

--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -2,6 +2,42 @@ import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma.service';
 import { User, Prisma } from '@prisma/client';
 
+export type UserProfileFull = {
+	id: string;
+	username: string;
+	email: string;
+	createdAt: Date;
+	avatar: string;
+	banner: string;
+	country: string;
+	level: number;
+	level_percentage: number;
+	wins: number;
+	losses: number;
+	matches: number;
+	rank: number;
+	division: string;
+	two_factor: boolean;
+};
+
+// a type which is a subset of UserProfileFull
+export type UserProfileMini = {
+	id: string;
+	username: string;
+	email: string;
+	createdAt: Date;
+	avatar: string;
+	banner: string;
+	country: string;
+	level: number;
+	level_percentage: number;
+	wins: number;
+	losses: number;
+	matches: number;
+	rank: number;
+	division: string;
+};
+
 @Injectable()
 export class UserService {
 	constructor(private prisma: PrismaService) { }
@@ -49,19 +85,53 @@ export class UserService {
 		}
 	}
 
-	async getProfileFull(username: string): Promise<User | null> {
-		return this.prisma.user.findUnique({
+	async getProfileFull(username: string): Promise<UserProfileFull | null> {
+		const user = await this.prisma.user.findUnique({
 			where: { username },
 		});
+		if (!user) return null;
+		// Find matches with ids in user.history
+		const matches = await this.prisma.gameMatch.findMany({
+			where: {
+				id: {
+					in: user.history,
+				},
+			},
+		});
+		const total_matches = matches.length;
+		const matches_won = matches.filter((match) => match.winner_id === user.id).length
+		return {
+			id: user.id,
+			username: user.username,
+			email: user.email,
+			createdAt: user.created_at,
+			avatar: user.avatar,
+			banner: user.banner,
+			country: user.country,
+			level: user.level,
+			level_percentage: user.level_exp * 100 / 1000,
+			matches: matches.length,
+			wins: matches.filter((match) => match.winner_id === user.id).length,
+			losses: matches.filter((match) => match.winner_id !== user.id).length,
+			rank: user.rank,
+			division: user.division,
+			two_factor: user.two_factor,
+		};
 	}
 
-	async getProfileMini(username: string): Promise<User | null> {
-		const user = this.getProfileFull(username);
-		return user;
+	async getProfileMini(username: string): Promise<UserProfileMini | null> {
+		const user = await this.getProfileFull(username);
+		const { two_factor, ...rest } = user;
+		return {
+			...rest,
+		}
 	}
 
-	async getProfileMicro(username: string): Promise<User | null> {
-		const user = this.getProfileFull(username);
-		return user;
+	async getProfileMicro(username: string): Promise<UserProfileMini | null> {
+		const user = await this.getProfileFull(username);
+		const { two_factor, ...rest } = user;
+		return {
+			...rest,
+		}
 	}
 }
