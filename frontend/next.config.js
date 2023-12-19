eb38afc60334de0377ea90007897ceeb5a61b6d1
Seikirin <seikirin.tempest@gmail.com>
2023-12-19 10:22:44 +0100

    frontend env check

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+	env: {
+		NEXT_PUBLIC_BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL,
+	},
+}
 
 module.exports = nextConfig
