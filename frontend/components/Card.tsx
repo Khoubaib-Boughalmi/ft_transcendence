77540bfcdfb30c5a82a29e309e414c44b441c68a
Seikirin <seikirin.tempest@gmail.com>
2024-01-07 10:21:35 +0100

    added classnames prop to card component

--- frontend/components/Card.tsx
+++ frontend/components/Card.tsx
@@ -4,7 +4,15 @@ import Divider from "@/components/Divider";
 import { twMerge } from "tailwind-merge";
 
 export default function Card({
-	header, children, footer, color = "bg-card-300", fullWidth = false, className, skeleton, ...props
+	header,
+	children,
+	footer,
+	color = "bg-card-300",
+	fullWidth = false,
+	className,
+	skeleton,
+	classNames,
+	...props
 }: {
 	header?: ReactNode;
 	children?: ReactNode;
@@ -13,13 +21,17 @@ export default function Card({
 	fullWidth?: boolean;
 	className?: string;
 	skeleton?: ReactNode;
+	classNames?: { container?: string; innerContainer?: string };
 	[key: string]: any;
 }) {
 	return (
 		<div
 			{...props}
 			data-full-width={fullWidth}
-			className={twMerge(`flex flex-col rounded-3xl ${color} z-10 data-[full-width=true]:w-full`, className
+			className={twMerge(
+				`flex flex-col rounded-3xl ${color} z-10 data-[full-width=true]:w-full`,
+				className,
+				classNames?.container,
 			)}
 		>
 			{skeleton}
@@ -32,7 +44,14 @@ export default function Card({
 				</>
 			)}
 
-			<div className="flex-1 p-2 text-background-800">{children}</div>
+			<div
+				className={twMerge(
+					"flex-1 p-2 text-background-800",
+					classNames?.innerContainer,
+				)}
+			>
+				{children}
+			</div>
 
 			{footer && (
 				<>
