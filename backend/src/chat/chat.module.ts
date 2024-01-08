b33ce001205f375c9c95b56b14a2ce85641e7f9a
NotAsheraf <ash@rian.ma>
2024-01-08 10:44:52 +0100

    Add AppService as a provider to the chat module

--- backend/src/chat/chat.module.ts
+++ backend/src/chat/chat.module.ts
@@ -4,10 +4,11 @@ import { ChatService } from './chat.service';
 import { PrismaService } from 'src/prisma.service';
 import { NestjsFormDataModule } from 'nestjs-form-data';
 import { UserService } from 'src/user/user.service';
+import { AppService } from 'src/app.service';
 
 @Module({
   controllers: [ChatController],
-  providers: [ChatService, PrismaService, UserService],
+  providers: [ChatService, PrismaService, UserService, AppService],
   imports: [NestjsFormDataModule],
 })
 export class ChatModule {}
