eadb5c978fdb43b48c2d9c258dce3e0fbceb3447
Seikirin <seikirin.tempest@gmail.com>
2024-01-20 12:03:28 +0100

    added domains to next config for next/image

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
 	env: {
 		NEXT_PUBLIC_BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL,
 	},
+	images: {
+		domains: ['upload.wikimedia.org', 'cdn.abcotvs.com', 'i.natgeofe.com', 'pregonanto.s3.eu-west-3.amazonaws.com', 'static.vecteezy.com'],
+	},
 }
 
 module.exports = nextConfig
