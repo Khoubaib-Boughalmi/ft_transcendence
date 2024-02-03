5913c460e50139308adfedce2e3710f7a125bc96
NotAsheraf <ash@rian.ma>
2024-02-03 11:36:31 +0100

    Use remotePatterns instead of domains in next image config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,7 +4,11 @@ const nextConfig = {
 		NEXT_PUBLIC_BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL,
 	},
 	images: {
-		domains: ['upload.wikimedia.org', 'cdn.abcotvs.com', 'i.natgeofe.com', 'pregonanto.s3.eu-west-3.amazonaws.com', 'static.vecteezy.com', 'dslv9ilpbe7p1.cloudfront.net', 'i.ytimg.com']
+		remotePatterns: [
+			{
+				hostname: '*',
+			},
+		],
 	},
 }
 
