9c66d34d4f0cfda9a797e633993bf528bdd4b4b8
NotAsheraf <ash@rian.ma>
2024-02-24 15:54:41 +0100

    Animated long enough

--- frontend/components/LoginGuard.tsx
+++ frontend/components/LoginGuard.tsx
@@ -1,13 +1,28 @@
 "use client";
 import PublicContext from "@/contexts/PublicContext";
 import { Spinner } from "@nextui-org/react";
-import { useContext } from "react";
+import { useCallback, useContext, useEffect, useRef } from "react";
 import { Button } from "./Button";
+import { motion, stagger, useAnimate } from "framer-motion";
 
 export default function LoginGuard({ children }: any) {
 	const { verified, verifiedLoading, sessionLoading } = useContext(
 		PublicContext,
 	) as any;
+	const [scope, animate] = useAnimate();
+
+	const divRef = useCallback(() => {
+		animate([
+			["span", { opacity: 1 }, { duration: 2, delay: stagger(0.05) }],
+		]);
+		setTimeout(() => {
+			animate(
+				"span",
+				{ textShadow: "0px 0px 150px white" },
+				{ duration: 1.5 },
+			);
+		}, 1500);
+	}, []);
 
 	if (verifiedLoading)
 		return (
@@ -21,10 +36,22 @@ export default function LoginGuard({ children }: any) {
 			{verified ? (
 				<>{children}</>
 			) : (
-				<div className="z-10 flex select-none flex-col items-center justify-center gap-14 text-[0.75em]">
-					<div className="bg-gradient-to-t from-primary to-secondary-800 bg-clip-text text-[15em] text-transparent">
-						long enough
-					</div>
+				<div
+					ref={divRef}
+					className="z-10 flex select-none flex-col items-center justify-center gap-14 text-[0.75em]"
+				>
+					<motion.div ref={scope}>
+						{"long enough".split("").map((word, idx) => {
+							return (
+								<motion.span
+									key={word + idx}
+									className="bg-gradient-to-t from-primary to-secondary-800 bg-clip-text text-[15em] text-transparent opacity-0"
+								>
+									{word}
+								</motion.span>
+							);
+						})}
+					</motion.div>
 					<div className="-mt-12 text-[1.25em] font-light text-foreground-600">
 						An interactive gaming platform for the 1337 community.
 						All rights reserved 2023 ©.
