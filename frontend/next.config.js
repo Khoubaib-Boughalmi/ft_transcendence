4eb978b6018eed2a3209926271b21adb1b204175
Seikirin <seikirin.tempest@gmail.com>
2024-03-17 21:38:38 +0000

    Adding NEXT_PUBLIC_FRONTEND_URL as a mandatory variable

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
 	env: {
 		NEXT_PUBLIC_BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL,
+		NEXT_PUBLIC_FRONTEND_URL: process.env.NEXT_PUBLIC_FRONTEND_URL,
 	},
 	images: {
 		remotePatterns: [
