04599267e0234d61fa8f95c886b1be127ad792ad
echaoub <abdochaoubi2000@gmail.com>
2024-04-28 15:35:13 +0100

    Remove unused import statement

--- frontend/models/Table.tsx
+++ frontend/models/Table.tsx
@@ -1,7 +1,6 @@
 "use client";
 import React, { useRef } from "react";
 import { useGLTF } from "@react-three/drei";
-import { a } from "@react-spring/three";
 import * as THREE from "three";
 
 export function Table(props) {
