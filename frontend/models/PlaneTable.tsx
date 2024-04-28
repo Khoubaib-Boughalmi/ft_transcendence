4e3911ffd75f042624e112cd24d494df2a5b1ae6
echaoub <abdochaoubi2000@gmail.com>
2024-04-28 15:33:20 +0100

    Remove commented out code in PlaneTable component

--- frontend/models/PlaneTable.tsx
+++ frontend/models/PlaneTable.tsx
@@ -1,29 +1,16 @@
 import { PlaneProps, usePlane } from "@react-three/cannon";
 import React from "react";
-import { Ref } from "react";
 import { Mesh } from "three";
 
 export function PlaneTable(props: PlaneProps) {
 	const [ref, api] = usePlane<Mesh>(() => ({
 		name: "ground",
 		...props,
-		// onCollide: () => {
-		//   console.log("collide table");
-
-		//   // api.position.
-		//   // api.velocity.set(0, 1, 0);
-		//   //rotation ++
-		//   // api.quaternion.set(0, 0, 0, 0);
-		// },
 	}));
 
 	return (
-		// <group ref={ref} name="ground">
 		<mesh receiveShadow ref={ref} name="ground">
-			{/* <planeGeometry args={[10, 10]} /> */}
-			{/* <meshStandardMaterial color="#ff1f00" /> */}
 		</mesh>
-		// </group>
 	);
 }
 
